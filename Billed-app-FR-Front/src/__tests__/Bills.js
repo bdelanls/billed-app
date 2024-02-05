e9c9c423273a87b9fb977183f500d5a9d8726856
bdelanls <bdelanls@gmail.com>
2024-02-05 15:31:05 +0100

    intégration get

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,11 +5,14 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+// import Bills from "../containers/Bills.js"
 import { ROUTES_PATH, ROUTES} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
-
+import mockStore from "../__mocks__/store";
 import '@testing-library/jest-dom';
 import router from "../app/Router.js";
+
+// jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
